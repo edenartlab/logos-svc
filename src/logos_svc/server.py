8d5f1572f70f5c7f02b87303d6a63e9816c4547a
Jonathan Miller <jon@jmill.dev>
2023-12-20 09:07:23 -0800

    pydantic is dogshit

--- src/logos_svc/server.py
+++ src/logos_svc/server.py
@@ -1,8 +1,11 @@
-from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
 from logos.scenarios.eden_assistant import EdenAssistant
 from dotenv import load_dotenv
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+import logging
 
 load_dotenv()
 
@@ -12,14 +15,24 @@ app = FastAPI()
 class EdenAssistantInput(BaseModel):
     name: str
     identity: str
-    knowledge_summary: str
-    knowledge: str
+    knowledge_summary: Optional[str] = Field(None)
+    knowledge: Optional[str] = Field(None)
 
 
 class InteractionInput(BaseModel):
     prompt: str
     author_id: str
-    attachments: Optional[list]
+    attachments: Optional[List[str]] = Field(None)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    logging.error(f"{request}: {exc_str}")
+    content = {"status_code": 10422, "message": exc_str, "data": None}
+    return JSONResponse(
+        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
 
 
 @app.post("/interact")
