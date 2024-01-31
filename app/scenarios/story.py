b6573f62bc937bd4f9849534515c14f65a02ed73
Jonathan Miller <jon@jmill.dev>
2024-01-31 11:36:38 -0800

    patch fix

--- app/scenarios/story.py
+++ app/scenarios/story.py
@@ -9,39 +9,47 @@ from ..character import EdenCharacter
 from ..prompt_templates.cinema import (
     screenwriter_system_template,
     screenwriter_prompt_template,
-    #director_template,
-    #cinematographer_template,
+    # director_template,
+    # cinematographer_template,
 )
 
 
-def story(request: StoryRequest):    
+def story(request: StoryRequest):
     params = {"temperature": 1.0, "max_tokens": 4096, **request.params}
-    
+
     character_details = ""
     character_names = []
     for character_id in request.character_ids:
+        if character_id == "":
+            continue
         character = EdenCharacter(character_id)
         character_names.append(character.name)
         character_details += f"""---
     Name: {character.name}
     Description: {character.identity}
     
-    """   
-    
-    CharacterNameEnum = Enum("CharacterNameEnum", {
-        name: name for name in character_names
-    })
-        
+    """
+
+    CharacterNameEnum = Enum(
+        "CharacterNameEnum", {name: name for name in character_names}
+    )
+
     class DynamicStoryClip(StoryClip):
         """
         A single clip in a screenplay sequence
         """
-        character: Optional[CharacterNameEnum] = Field(description="Character name if voiceover mode is character, otherwise null (possible values: {})".format(", ".join(character_names)))
+
+        character: Optional[CharacterNameEnum] = Field(
+            description="Character name if voiceover mode is character, otherwise null (possible values: {})".format(
+                ", ".join(character_names)
+            )
+        )
 
     class DynamicStoryResult(BaseModel):
         """
         A screenplay consisting of a sequence of clips
         """
+
         clips: List[DynamicStoryClip] = Field(description="Clips in the sequence")
 
     prompt = screenwriter_prompt_template.substitute(
